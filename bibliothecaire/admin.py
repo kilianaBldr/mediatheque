cb4c5809c8a4b101091f25d2349d93f789c550ee
Kiliana Blondron <kiliana.bldr@gmailcom>
2024-10-21 15:54:43 +0200

    Ajout de l'admin Django

--- bibliothecaire/admin.py
+++ bibliothecaire/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from .models import Membre, Media, Emprunt
 
-# Register your models here.
+
+admin.site.register(Membre)
+admin.site.register(Media)
+admin.site.register(Emprunt)
+
