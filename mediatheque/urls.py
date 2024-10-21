193f88d6e2bbd969c9b9ca755bf87ee9ddbed05a
Kiliana Blondron <kiliana.bldr@gmailcom>
2024-10-21 14:51:15 +0200

    Ajout de la page html de l'app membre

--- mediatheque/urls.py
+++ mediatheque/urls.py
@@ -22,4 +22,5 @@ from django.urls import path, include
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('bibliothecaire.urls')),
+    path('membre/', include('membre.urls'))
 ]
