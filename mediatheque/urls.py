afcffa1c91e1817365a868e800508034dd7af182
Kiliana Blondron <kiliana.bldr@gmailcom>
2024-10-21 14:36:15 +0200

    Ajout des fichiers HTML de l'app bibliothecaire

--- mediatheque/urls.py
+++ mediatheque/urls.py
@@ -20,6 +20,6 @@ from django.urls import path, include
 
 
 urlpatterns = [
-    path('bibliothecaire/', include('bibliothecaire.urls')),
     path('admin/', admin.site.urls),
+    path('', include('bibliothecaire.urls')),
 ]
